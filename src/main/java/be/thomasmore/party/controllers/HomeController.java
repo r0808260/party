1afe7b280b861b9f13426289d20ccb8653ce9ba4
r0808260 <r0808260@student.thomasmore.be>
2021-02-21 08:46:51 +0100

    exercise 3.2

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 @Controller
 public class HomeController {
     private final int mySpecialNumber = 729;
@@ -17,5 +20,10 @@ public class HomeController {
     public String about(Model model) {
         model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "about";
+    }
+    @GetMapping("/pay")
+    public String pay(Model model){
+        model.addAttribute("orderDate", LocalDate.now());
+        return "pay";
     }
 }
