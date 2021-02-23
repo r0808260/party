6c6ffd64cb3c6b62c16f858f752ec4b3c2885934
r0808260 <r0808260@student.thomasmore.be>
2021-02-23 09:28:36 +0100

    bundel 1

--- src/main/java/be/thomasmore/party/controllers/HomeController.java
+++ src/main/java/be/thomasmore/party/controllers/HomeController.java
@@ -4,26 +4,39 @@ package be.thomasmore.party.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
-    private final int mySpecialNumber = 729;
+    private final String[] venueNames = {"De Loods", "De Club", "De Hangar", "Zapoi", "Kuub", "Cuba Libre"};
+
     @GetMapping({"/", "/home"})
     public String home(Model model) {
-        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "home";
     }
+
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("mySpecialNumber", mySpecialNumber);
         return "about";
     }
-    @GetMapping("/pay")
-    public String pay(Model model){
-        model.addAttribute("orderDate", LocalDate.now());
-        return "pay";
+
+    @GetMapping({"/venuedetails", "/venuedetails/{index}"})
+    public String venueDetails(Model model,
+                               @PathVariable(required = false)  Integer index) {
+        if (index!=null && index>=0 && index<venueNames.length ) {
+            model.addAttribute("venueName", venueNames[index]);
+            model.addAttribute("prevIndex", index>0 ? index-1 : venueNames.length-1);
+            model.addAttribute("nextIndex", index<venueNames.length-1 ? index+1 : 0);
+        }
+        return "venuedetails";
+    }
+
+    @GetMapping("/venuelist")
+    public String venueList(Model model) {
+        model.addAttribute("venueNames", venueNames);
+        return "venuelist";
     }
 }
